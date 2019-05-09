417e907ba88090b4b98b8392ac73453ab16a5ae0
root-11 <bjorn.h.madsen@gmail.com>
2019-05-09 13:47:07 +0100

    added long description to setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,27 @@ MasLite
 """
 from datetime import datetime
 from setuptools import setup
+from os import path
 
 v = datetime.now()
 __version__ = "{}.{}.{}.{}".format(v.year, v.month, v.day, v.hour * 3600 + v.minute*60 + v.second)
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name="MASlite",
     version=__version__,
-    url="https://github.com/root-11/outscale",
+    url="https://github.com/root-11/maslite",
     license="MIT",
     author="Bjorn Madsen",
     author_email="bjorn.madsen@operationsresearchgroup.com",
-    description="A framework for fast multiprocessing multi-agent systems",
+    description="A lightweight multi-agent system",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="multi agent system MAS",
     packages=["maslite"],
     platforms="any",
     install_requires=[],
