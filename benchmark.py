4503c38be2dd6b20e6d98977bca224968f2801a5
root-11 <root-11@users.noreply.github.com>
2024-01-31 15:17:56 +0000

    Added benchmarks for messages / sec.

---
+++ benchmark.py
@@ -0,0 +1,37 @@
+from maslite import Agent, Scheduler, AgentMessage
+
+class Msg(AgentMessage):
+    def __init__(self, sender, receiver=None, topic=None):
+        super().__init__(sender, receiver, topic)
+        self.value = 0
+
+class A(Agent):
+    def __init__(self, uuid=None):
+        super().__init__(uuid)
+        
+    def update(self):
+        if self.messages:
+            m = self.receive()
+            assert isinstance(m, Msg)
+            m.value += 1
+            m.receiver, m.sender = m.sender, m.receiver
+            self.send(m)
+
+
+if __name__ == "__main__":
+    s = Scheduler()
+    a = A()
+    b = A()
+    s.add(a)
+    s.add(b)
+    m = Msg(a,b)
+    a.send(m)
+    s.run(seconds=10)
+    print(f"{m.value/10:,} messages/second")
+
+    # :~$ python3.10 benchmarks.py
+    # 340,283.8 messages/second
+
+    # :-$ pypy3 benchmarks.py
+    # 2,975,577.7 messages/second
+    
